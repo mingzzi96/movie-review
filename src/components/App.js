3de5c246a174f67352ddebe97231d435d11dd089
mingming <jimin23623@gmail.com>
2023-12-30 22:55:38 +0900

    ✨ FEAT: 삭제 기능 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import ReviewList from "./ReviewList";
-import items from "../mock.json";
+import mockItems from "../mock.json";
 import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState(mockItems);
   const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
   const handleBestClick = () => setOrder("rating");
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
   return (
     <>
       <div>
@@ -15,7 +20,7 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <div>
-        <ReviewList items={sortedItems} />
+        <ReviewList items={sortedItems} onDelete={handleDelete} />
       </div>
     </>
   );
