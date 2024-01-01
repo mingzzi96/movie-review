aa4b361e5d058af4cd5c19c241fca1c974be6258
mingming <jimin23623@gmail.com>
2024-01-01 21:58:30 +0900

    ♻️ REFACTOR: 여러 state를 하나로 뭉침

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -2,27 +2,36 @@ import { useState } from "react";
 import "./ReviewForm.css";
 
 function ReviewForm() {
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+  const handleChange = (e) => {
+    // target input의 name 프로퍼티와 해당 프로퍼티 안의 value값을 가져와서 저장.
+    const { name, value } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   };
 
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
   };
   return (
-    <form className="ReviewForm">
-      <input value={title} onChange={handleTitleChange} />
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange} />
+    <form className="ReviewForm" onSubmit={handleSubmit}>
+      <input name="title" value={values.title} onChange={handleChange} />
+      <input
+        name="rating"
+        type="number"
+        value={values.rating}
+        onChange={handleChange}
+      />
+      <textarea name="content" value={values.content} onChange={handleChange} />
+      <button type="submit">확인</button>
     </form>
   );
 }
