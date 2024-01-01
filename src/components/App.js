6c4ef57ad1e03fd5f8e8e2e51608b02a7e68e162
mingming <jimin23623@gmail.com>
2024-01-01 19:45:59 +0900

    ✨ FEAT: 더보기 버튼 기능 추가

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import ReviewList from "./ReviewList";
 import { useEffect, useState } from "react";
 import { getReviews } from "../api/api";
 
+const LIMIT = 6;
+
 function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
@@ -13,12 +17,25 @@ function App() {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
-  const handleLoad = async (orderQuery) => {
-    const { reviews } = await getReviews(orderQuery);
-    setItems(reviews);
+
+  const handleLoad = async (options) => {
+    const { reviews, paging } = await getReviews(options);
+    console.log(reviews);
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems([...items, ...reviews]);
+    }
+    setOffset(options.offset + reviews.length);
+    setHasNext(paging.hasNext);
   };
+
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: LIMIT });
+  };
+
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({ order, offset: 0, limit: LIMIT });
   }, [order]);
   return (
     <>
@@ -28,6 +45,9 @@ function App() {
       </div>
       <div>
         <ReviewList items={sortedItems} onDelete={handleDelete} />
+        <button disabled={!hasNext} onClick={handleLoadMore}>
+          + 더 보기
+        </button>
       </div>
     </>
   );
