814b55bcaacb43d2ce0cf9d0e01fff50f6ee462e
mingming <jimin23623@gmail.com>
2024-01-02 16:27:53 +0900

    ✨ FEAT: 삭제 기능 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import ReviewList from "./ReviewList";
 import { useEffect, useState } from "react";
-import { createReview, getReviews, updateReview } from "../api/api";
+import {
+  createReview,
+  deleteReview,
+  getReviews,
+  updateReview,
+} from "../api/api";
 import ReviewForm from "./ReviewForm";
 
 const LIMIT = 6;
@@ -17,9 +22,11 @@ function App() {
 
   const handleNewestClick = () => setOrder("createdAt");
   const handleBestClick = () => setOrder("rating");
-  const handleDelete = (id) => {
-    const nextItems = items.filter((item) => item.id !== id);
-    setItems(nextItems);
+  const handleDelete = async (id) => {
+    const result = await deleteReview(id);
+    if (!result) return;
+
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
   };
 
   const handleLoad = async (options) => {
