b5fd295fbb6158b07a2f55aefd15719170bfd862
mingming <jimin23623@gmail.com>
2024-01-02 14:59:18 +0900

    ✨ FEAT: 별점 선택하기 기능 추가

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./ReviewForm.css";
 import FileInput from "./FileInput";
+import RatingInput from "./RatingInput";
 
 function ReviewForm() {
   const [values, setValues] = useState({
@@ -29,11 +30,11 @@ function ReviewForm() {
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
       <input name="title" value={values.title} onChange={handleInputChange} />
-      <input
+      <RatingInput
         name="rating"
         type="number"
         value={values.rating}
-        onChange={handleInputChange}
+        onChange={handleChange}
       />
       <FileInput
         name="imgFile"
