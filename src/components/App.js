fa2773dff0d99294041e28a62f30c5fcd03a09b3
mingming <jimin23623@gmail.com>
2024-01-03 15:54:17 +0900

    ♻️ REFACTOR: useCallback의 사용

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import ReviewList from "./ReviewList";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {
   createReview,
   deleteReview,
@@ -29,20 +29,23 @@ function App() {
     setItems((prevItems) => prevItems.filter((item) => item.id !== id));
   };
 
-  const handleLoad = async (options) => {
-    const result = await getReviewsAsync(options);
+  const handleLoad = useCallback(
+    async (options) => {
+      const result = await getReviewsAsync(options);
 
-    if (!result) return;
+      if (!result) return;
 
-    const { reviews, paging } = result;
-    if (options.offset === 0) {
-      setItems(reviews);
-    } else {
-      setItems((prevItems) => [...prevItems, ...reviews]);
-    }
-    setOffset(options.offset + reviews.length);
-    setHasNext(paging.hasNext);
-  };
+      const { reviews, paging } = result;
+      if (options.offset === 0) {
+        setItems(reviews);
+      } else {
+        setItems((prevItems) => [...prevItems, ...reviews]);
+      }
+      setOffset(options.offset + reviews.length);
+      setHasNext(paging.hasNext);
+    },
+    [getReviewsAsync]
+  );
 
   const handleLoadMore = () => {
     handleLoad({ order, offset, limit: LIMIT });
@@ -65,7 +68,8 @@ function App() {
 
   useEffect(() => {
     handleLoad({ order, offset: 0, limit: LIMIT });
-  }, [order]);
+  }, [order, handleLoad]);
+
   return (
     <>
       <div>
--- src/components/hooks/useAsync.js
+++ src/components/hooks/useAsync.js
@@ -1,20 +1,24 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 function useAsync(asyncFunction) {
   const [pending, setPending] = useState(false);
   const [error, setError] = useState(null);
-  const wrappedFunction = async (...args) => {
-    try {
-      setError(null);
-      setPending(true);
-      return await asyncFunction(...args);
-    } catch (error) {
-      setError(error);
-      return;
-    } finally {
-      setPending(false);
-    }
-  };
+
+  const wrappedFunction = useCallback(
+    async (...args) => {
+      try {
+        setError(null);
+        setPending(true);
+        return await asyncFunction(...args);
+      } catch (error) {
+        setError(error);
+        return;
+      } finally {
+        setPending(false);
+      }
+    },
+    [asyncFunction]
+  );
 
   return [pending, error, wrappedFunction];
 }
